18e0d496337b5bed86945ba1308f3e177fe6571a
Tyler Cornelius <tyler.cornelius14@gmail.com>
2021-02-07 20:57:03 -0700

    Update iteration 2

--- src/order.js
+++ src/order.js
@@ -1,7 +1,22 @@
 
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders = deliveryOrders.push(order);
+  }
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+}
+
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
